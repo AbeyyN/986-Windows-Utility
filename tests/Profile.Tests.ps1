b604a07fcef5a122cbe0879e28a70f042bfc90e8
AbeyymnzrL <86111714+AbeyyN@users.noreply.github.com>
2026-09-10 00:31:48 +0800

    Make profile tests temp-path portable

--- tests/Profile.Tests.ps1
+++ tests/Profile.Tests.ps1
@@ -23,7 +23,7 @@ foreach ($term in $forbidden) {
 $ids = [regex]::Matches($appText,"Id='([^']+)'\s*; Category=") | ForEach-Object { $_.Groups[1].Value }
 $balancedIds = [regex]::Matches($appText,'Id=''([^'']+)''[^\r\n]+Balanced=\$true') | ForEach-Object { $_.Groups[1].Value }
 
-$StateDir = Join-Path $env:TEMP ('986-profile-test-' + [guid]::NewGuid().ToString('N'))
+$StateDir = Join-Path ([IO.Path]::GetTempPath()) ('986-profile-test-' + [guid]::NewGuid().ToString('N'))
 function Write-AppLog([string]$Message) { }
 . $module
 
