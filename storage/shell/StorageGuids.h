4722804c67f088265742885e838f9de108c57612
AbeyymnzrL <86111714+AbeyyN@users.noreply.github.com>
2026-09-13 02:25:55 +0800

    Pin 986 Storage shell CLSID

---
+++ storage/shell/StorageGuids.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <guiddef.h>
+
+// Immutable CLSID for 986 Storage.
+// {5FCCE720-D806-4B6A-A5F1-F060344FC88D}
+DEFINE_GUID(CLSID_986Storage,
+0x5fcce720, 0xd806, 0x4b6a, 0xa5, 0xf1, 0xf0, 0x60, 0x34, 0x4f, 0xc8, 0x8d);
+
+static const wchar_t k986StorageClsidString[] = L"{5FCCE720-D806-4B6A-A5F1-F060344FC88D}";
+static const wchar_t k986StorageTitle[] = L"986 Storage";
